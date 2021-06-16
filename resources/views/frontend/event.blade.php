c8f695e4bb6b04e0fa27e41f5abc2244433a25bf
Md. Sakir Ahmed <52326976+mdsakirahmed@users.noreply.github.com>
2021-06-16 12:48:40 +0600

    event style

--- resources/views/frontend/event.blade.php
+++ resources/views/frontend/event.blade.php
@@ -35,7 +35,7 @@
                 <div class="col-12">
                     <div class="section-title text-center mb-4 pb-2">
                         <span class="badge badge-pill badge-soft-primary">Upcoming Events</span>
-                        <h4 class="title mt-3 mb-4">Find Your Specialists</h4>
+                        <h4 class="title mt-3 mb-4">Upcoming Events</h4>
                         <p class="text-muted mx-auto para-desc mb-0">Great doctor if you need your family member to get effective immediate assistance, emergency treatment or a simple consultation.</p>
                     </div>
                 </div><!--end col-->
@@ -43,7 +43,7 @@
 
             <div class="row align-items-center">
                 <div class="col-xl-3 col-lg-3 col-md-6 mt-4 pt-2 d-flex align-items-stretch">
-                    <div class="card team border-0 rounded shadow overflow-hidden">
+                    <div class="card team border-0 shadow-lg p-3 mb-5 bg-white rounded overflow-hidden text-center">
                         <div class="team-person position-relative overflow-hidden">
                             <img src="images/logo-black.png" class="img-fluid" alt="">
                         </div>
@@ -52,22 +52,48 @@
                                 05 - 06 AUGUST 2021
                             </div>
                         </div>
-                        <div class="card-body">
+                        <div class="card-body text-center">
                             <a href="#" class="title text-dark h5 d-block mb-0">
                                 WORLD SOCIAL INNOVATION FORUM – ASIA MEETING
                             </a>
                             <ul class="list-unstyled mt-2 mb-0">
-                                <li class="d-flex">
-                                    <i class="ri-map-pin-line text-primary align-middle"></i>
+                                <li class="">
+                                    <i class="ri-map-pin-line text-primary"></i>
                                     <small class="text-muted ms-2">Virtual</small>
                                 </li>
                             </ul>
                             <ul class="list-unstyled mt-2 mb-0 text-center">
-                                <a href="#" class="btn btn-pills btn-soft-primary"> Primary </a>
+                                <a href="#" class="btn btn-pills btn-soft-primary"> VIEW DETAILS </a>
                             </ul>
                         </div>
                     </div>
-                </div><!--end col-->
+                </div>
+                <div class="col-xl-3 col-lg-3 col-md-6 mt-4 pt-2 d-flex align-items-stretch">
+                    <div class="card team border-0 shadow-lg p-3 mb-5 bg-white rounded overflow-hidden text-center">
+                        <div class="team-person position-relative overflow-hidden">
+                            <img src="images/logo-black.png" class="img-fluid" alt="">
+                        </div>
+                        <div class="row mt-3 mb-3">
+                            <div class="col text-center text-white bg-primary">
+                                08 - 09 NOVEMBER 2021
+                            </div>
+                        </div>
+                        <div class="card-body text-center">
+                            <a href="#" class="title text-dark h5 d-block mb-0">
+                                WORLD SOCIAL INNOVATION FORUM – ANNUAL MEETING
+                            </a>
+                            <ul class="list-unstyled mt-2 mb-0">
+                                <li class="">
+                                    <i class="ri-map-pin-line text-primary"></i>
+                                    <small class="text-muted ms-2">Singapore, Singapore</small>
+                                </li>
+                            </ul>
+                            <ul class="list-unstyled mt-2 mb-0 text-center">
+                                <a href="#" class="btn btn-pills btn-soft-primary"> VIEW DETAILS </a>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
             </div><!--end row-->
         </div><!--end container-->
     </section>
